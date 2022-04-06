43069b545eae37ad78a7717a763100a37ad33720
Lincoln Ruteski <lruteski@gmail.com>
2022-04-06 16:14:06 -0300

    primeiro commit

---
+++ 01_intro/index.js
@@ -0,0 +1,9 @@
+var nome = "Lincoln";
+var sobrenome = 1;
+var x = true;
+function greetings(nome) {
+    console.log("Olá, ", nome);
+}
+greetings(nome);
+// greetings(sobrenome);
+// greetings(x);
---
+++ 01_intro/index.ts
@@ -0,0 +1,11 @@
+const nome: string = "Lincoln";
+const sobrenome: number = 1;
+const x: boolean = true;
+
+function greetings(nome: string) {
+  console.log("Olá, ", nome);
+}
+
+greetings(nome);
+// greetings(sobrenome);
+// greetings(x);
