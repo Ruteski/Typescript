015705b6819616435fe709201dd593dd4eea2e29
Lincoln Ruteski <lruteski@gmail.com>
2022-04-08 16:52:24 -0300

    criando context Api

--- 11_react_com_ts/src/App.tsx
+++ 11_react_com_ts/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {createContext} from 'react';
 import FirstComponent from './components/FirstComponent'
 import SecondComponent from './components/SecondeComponent'
 import Destructoring, {Category} from './components/Destructoring'
@@ -6,6 +6,14 @@ import State from './components/State'
 
 type textOrNull = string | null
 type fixed = 'isso' | 'ou' | 'aquilo'
+
+interface IAppContext {
+  language: string,
+  framework: string,
+  projects: number
+}
+
+export const AppContext = createContext<IAppContext | null>(null)
 
 function App() {
   const name: string = 'Lincoln'
@@ -23,45 +31,53 @@ function App() {
     return `Ola, ${name}`
   }
 
+  const contextValue: IAppContext = { 
+    language: 'javascript',
+    framework: 'express',
+    projects: 5
+  }
+
   return (
-    <div className="App">
-      <h1>Typescript com react</h1>
-      <h2>Nome: {name}</h2>
-      <h2>Idade: {age}</h2>
-      {isWorking && (
-        <p>Esta trabalhando</p>
-      )}
+    <AppContext.Provider value={contextValue}>
+      <div className="App">
+        <h1>Typescript com react</h1>
+        <h2>Nome: {name}</h2>
+        <h2>Idade: {age}</h2>
+        {isWorking && (
+          <p>Esta trabalhando</p>
+        )}
 
-      <h3>{userGreeting(name)}</h3>
-      <FirstComponent />
-      <SecondComponent name="Segundo" />
-      
-      <Destructoring 
-        title='Primeiro Post'
-        content='Algum Conteudo'
-        commentsQty={10}
-        tags={['ts', 'js']}
-        category={Category.TS}
-      />
+        <h3>{userGreeting(name)}</h3>
+        <FirstComponent />
+        <SecondComponent name="Segundo" />
+        
+        <Destructoring 
+          title='Primeiro Post'
+          content='Algum Conteudo'
+          commentsQty={10}
+          tags={['ts', 'js']}
+          category={Category.TS}
+        />
 
-      <Destructoring 
-        title='Segundo Post'
-        content='Mais outro Conteudo'
-        commentsQty={5}
-        tags={['python']}
-        category={Category.P}
-      />
+        <Destructoring 
+          title='Segundo Post'
+          content='Mais outro Conteudo'
+          commentsQty={5}
+          tags={['python']}
+          category={Category.P}
+        />
 
-      <State />
+        <State />
 
-      {myText && (
-        <p>tem texto na variavel</p>
-      )}
+        {myText && (
+          <p>tem texto na variavel</p>
+        )}
 
-      {mySecondText && (
-        <p>tem texto na variavel</p>
-      )}
-    </div>
+        {mySecondText && (
+          <p>tem texto na variavel</p>
+        )}
+      </div>
+    </AppContext.Provider>
   );
 }
 
